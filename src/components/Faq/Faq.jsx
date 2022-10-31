e8e61377afdd54811476166703bf2bc047cbe46a
Floppster <roman.mynamemail@gmail.com>
2022-11-01 00:40:15 +0700

    Add Auto Close onClick another tab

--- src/components/Faq/Faq.jsx
+++ src/components/Faq/Faq.jsx
@@ -5,19 +5,35 @@ import "./Faq.scss";
 const Faq = ({ faq_a, faq_p }) => {
   const symbolState = useRef();
   const tabP = useRef();
+
   function openTab(e) {
     tabP.current.classList.toggle("none");
-
     if (symbolState.current.innerHTML === "+") {
       symbolState.current.innerHTML = "-";
     } else {
       symbolState.current.innerHTML = "+";
     }
   }
+  function closeAll() {
+    const tabPAll = document.querySelectorAll(".faq__p"),
+      symbolStateAll = document.querySelectorAll(".faq__a__div__a");
+    tabPAll.forEach((element) => {
+        element.classList.add("none");
+    });
+    symbolStateAll.forEach((element) => {
+      element.innerHTML = "+";
+    });
+  }
 
   return (
     <div className="faq">
-      <a className="faq__a" onClick={(e) => openTab(e)}>
+      <a
+        className="faq__a"
+        onClick={(e) => {
+          closeAll(e);
+          openTab(e);
+        }}
+      >
         <div className="faq__a__div">
           <p>{faq_a}</p>
           <a ref={symbolState} className="faq__a__div__a">
